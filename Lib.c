ffa232ed21bd533ce5e751ee178f86eec100c25f
Frind@Mac@Docker@Ubuntu <49829681a@gmail.com>
2021-03-27 11:49:14 +0000

    Linker 7.7

--- Lib.c
+++ Lib.c
@@ -4,5 +4,12 @@
 void foobar(int i)
 {
 	printf("Printing fromd Lib.so %d\n",i);
-	sleep(-1);
+	sleep(10);
+}
+
+void __attribute__((constructor)) init_function(void){
+	printf("Initialize Dynamic Library\n");
+}
+void __attribute__((destructor)) fini_function(void){
+	printf("Closing Dynamic Library\n");
 }
--- Lib.h
+++ Lib.h
@@ -1,1 +1,3 @@
 void foobar(int);
+void __attribute__((constructor)) init_function(void);
+void __attribute__((destructor)) fini_function(void);
